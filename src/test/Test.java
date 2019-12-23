b9bd810da05b3655362a44dad443a78325e74060
MANOJ <krishnamanoj247@gmail.com>
2019-12-23 14:46:17 -0700

    updated filesnames and path

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,35 +6,29 @@ import util.FileReader;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         long time = System.currentTimeMillis();
         FileWriter fileWriter = null;
         try {
-            fileWriter = new FileWriter("test.txt");
+            fileWriter = new FileWriter("test_output.txt");
             for (int i = 1; i <= 52; i++) {
-
                 //Step 1: Read input file
                 String filename = "input" + i + ".txt";
-                FileReader fileReader = new FileReader("G:\\Masters CS\\Study\\Fall 2019\\Artificial Intelligence\\Homework\\Homework3\\homework3\\resources\\" + filename);
+                FileReader fileReader = new FileReader(".\\resources\\" + filename);
                 Input input = fileReader.readFile();
 
                 //Step 2: Perform Resolution
                 KnowledgeBasedAgent agent = new KnowledgeBasedAgent();
                 List<String> output = agent.resolution(input);
 
-
+                //Step 3: Write to the test output file
                 fileWriter.write("\nThe output for " + i);
                 for (String string : output) {
                     fileWriter.write("\n" + string);
                 }
-                Set<Integer> set = new LinkedHashSet<>();
-
-
             }
         } catch (Exception e) {
             e.printStackTrace();
